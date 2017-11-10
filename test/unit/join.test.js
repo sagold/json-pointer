e80262bcf16c767a4fa7f9752746a7b2f7b23de3
Sascha Goldhofer <post@saschagoldhofer.de>
2017-11-10 02:42:20 +0100

    Refactor to es6

--- test/unit/join.test.js
+++ test/unit/join.test.js
@@ -1,65 +1,57 @@
 "use strict";
 
 
-var expect = require("chai").expect;
-var pointer = require("../../lib");
-var join = pointer.join;
+const expect = require("chai").expect;
+const pointer = require("../../lib");
+const join = pointer.join;
 
 
-describe("pointer.join()", function () {
+describe("pointer.join()", () => {
 
-	it("should return a json pointer", function () {
-
-		var pointer = join();
+	it("should return a json pointer", () => {
+		const pointer = join();
 
 		expect(pointer).to.eq("#");
 	});
 
-	it("should join multiple pointers", function () {
-
-		var pointer = join("#/parent", "#/child", "#/target");
-
-		expect(pointer).to.eq("#/parent/child/target");
-	});
-
-	it("should join single words to a pointer", function () {
-
-		var pointer = join("parent", "child", "target");
+	it("should join multiple pointers", () => {
+		const pointer = join("#/parent", "#/child", "#/target");
 
 		expect(pointer).to.eq("#/parent/child/target");
 	});
 
-	it("should not have multiple slashes", function () {
+	it("should join single words to a pointer", () => {
+		const pointer = join("parent", "child", "target");
 
-		var pointer = join("#", "", "first");
+		expect(pointer).to.eq("#/parent/child/target");
+	});
+
+	it("should not have multiple slashes", () => {
+		const pointer = join("#", "", "first");
 
 		expect(pointer).to.eq("#/first");
 	});
 
-	it("should resolve relative pointers", function () {
-
-		var pointer = join("#/first/second/third", "../../2");
+	it("should resolve relative pointers", () => {
+		const pointer = join("#/first/second/third", "../../2");
 
 		expect(pointer).to.eq("#/first/2");
 	});
 
-	it("should return parent pointer", function () {
-
-		var pointer = join("#/first/second/third", "..");
+	it("should return parent pointer", () => {
+		const pointer = join("#/first/second/third", "..");
 
 		expect(pointer).to.eq("#/first/second");
 	});
 
-	it("should return root pointer", function () {
-
-		var pointer = join("#/first", "..");
+	it("should return root pointer", () => {
+		const pointer = join("#/first", "..");
 
 		expect(pointer).to.eq("#");
 	});
 
-	it("should return always return root", function () {
-
-		var pointer = join("#", "..");
+	it("should return always return root", () => {
+		const pointer = join("#", "..");
 
 		expect(pointer).to.eq("#");
 	});
