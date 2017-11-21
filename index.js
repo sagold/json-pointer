d7eaedf81c0c0f5f9a844fb89a66b3ef50ac8ffe
Sascha Goldhofer <post@saschagoldhofer.de>
2017-11-21 12:36:50 +0100

    Add helper isRoot

--- index.js
+++ index.js
@@ -6,3 +6,5 @@ exports.set = require("./lib/set");
 exports.delete = require("./lib/delete");
 exports.join = require("./lib/join");
 exports.split = require("./lib/split");
+
+exports.isRoot = require("./lib/isRoot");
